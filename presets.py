3ce078c6d416dda48f250cc84e203238baed68f0
Xuxuh15 <xuxuhxv@gmail.com>
2025-07-29 19:25:05 +0200

    Add custom preset constants

---
+++ presets.py
@@ -0,0 +1,58 @@
+
+presets = {
+    '✅': {
+        'code': 'amazing', 
+        'sleep_quality': 8,
+        'soreness_score': 8,
+        'mood': 8,
+        'energy':8,
+        'performance': 8, 
+        'muscle_groups': [], 
+        'comment': ''
+    }, 
+    '👍':{
+        'code': 'very good', 
+        'sleep_quality': 7,
+        'soreness_score': 7,
+        'mood': 7,
+        'energy':7,
+        'performance': 7, 
+        'muscle_groups': [], 
+        'comment': ''
+    }, 
+    '👌': 
+    {
+        'code': 'ok', 
+        'sleep_quality': 7,
+        'soreness_score': 6,
+        'mood': 7,
+        'energy':6,
+        'performance': 6, 
+        'muscle_groups': [], 
+        'comment': ''
+    }, 
+    '👎': {
+        'code': 'bad', 
+        'sleep_quality': 6,
+        'soreness_score': 5,
+        'mood': 7,
+        'energy':6,
+        'performance': 5, 
+        'muscle_groups': [], 
+        'comment': ''
+    }, 
+    '❌': {
+        'code': 'very bad', 
+        'sleep_quality': 6,
+        'soreness_score': 4,
+        'mood': 6,
+        'energy':6,
+        'performance': 5, 
+        'muscle_groups': [], 
+        'comment': ''
+    }
+
+}
+
+
+
